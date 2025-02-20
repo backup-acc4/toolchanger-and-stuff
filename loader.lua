3b46c42f71964f93d5dec113d9114c802717d7ef
backup-acc4 <kevins@bloomsbury.ac.th>
2025-02-20 15:02:44 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -17,7 +17,7 @@ end
 local function send(text)
 	local StarterGui = game:GetService("StarterGui")
 	StarterGui:SetCore("SendNotification",{
-		Title = "loaded (made by backup-acc4)", -- You can remove this, idc
+		Title = "TOOLCHANGER v2 thing made by backup-acc4", -- You can remove this, idc
 		Text = text,
 		Duration = 5
 	})
